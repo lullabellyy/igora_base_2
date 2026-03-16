31e6b6680eda149e6191e8f752a4a8395fd7af8a
darya <125028255+lullabellyy@users.noreply.github.com>
2026-03-16 22:04:32 +0300

    send.js

--- api/send.js
+++ api/send.js
@@ -1,26 +1,25 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
- process.env.SUPABASE_URL,
- process.env.SUPABASE_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
 )
 
 export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" })
+  }
 
- if (req.method !== "POST") {
-  return res.status(405).json({ error: "Method not allowed" })
- }
+  const { name, phone } = req.body
 
- const { name, phone } = req.body
+  const { error } = await supabase
+    .from('feedback')
+    .insert([{ name, phone }])
 
- const { error } = await supabase
-  .from('feedback')
-  .insert([{ name, phone }])
+  if (error) {
+    console.log(error)   // выведет ошибку в лог Vercel
+    return res.status(500).json({ error: error.message })
+  }
 
- if (error) {
-  return res.status(500).json({ error: error.message })
- }
-
- res.status(200).json({ success: true })
-
+  res.status(200).json({ success: true })
 }
